1a194bbd042eb69ac4effb11c37ef5338d05979b
Mark Rasavong <mark.rasavong@gmail.com>
2022-04-21 17:20:29 +0200

    regex apr 21

---
+++ april/apr-21/index.js
@@ -0,0 +1,40 @@
+// abcdefghijklmnopqrstuvwxyz
+
+// ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
+//1234567890
+
+// Ha HaHa
+
+// MetaCharacter (Need to be escaped)
+// .[{()\^$|?*+,-.
+
+/*
+MUST BE ESCAPED WITH A BACKSLASH "\"
+\. => searches for periods
+\\ => search for a backslash
+*/
+
+// coreyms.com
+
+/*
+coreyms\.com
+*/
+
+// 321-555-4321
+
+// 123.555.1234
+
+/*
+
+Mr. Schafer
+
+Mr Smith
+
+Ms Davis
+
+Mrs. Robinson
+
+Mr. T
+
+*/
