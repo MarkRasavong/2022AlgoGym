8fd136216fe7f8dfd74c86691935f42fd21490ee
Mark Rasavong <rasavong.mark@gmail.com>
2022-04-24 10:41:47 +0200

    algoGym apr-24

--- april/apr-23/index.ts
+++
@@ -1,8 +0,0 @@
-//6kyu https://www.codewars.com/kata/523f5d21c841566fde000009/train/javascript
-
-const arrayDiff = (a: number[], b: number[]): number[] => {
-  if (a.length === 0) return a;
-  if (b.length === 0) return a;
-
-  return a.filter((num) => !b.includes(num));
-};
---
+++ april/apr-24/index.js
@@ -0,0 +1,21 @@
+"use strict";
+//6kyu https://www.codewars.com/kata/5277c8a221e209d3f6000b56/train/javascript
+function validBraces(braces) {
+    var matches = { '(': ')', '{': '}', '[': ']' };
+    var stack = [];
+    var currentChar;
+
+    for (var i = 0; i < braces.length; i++) {
+        currentChar = braces[i];
+
+        if (matches[currentChar]) { // opening braces
+            stack.push(currentChar);
+        } else { // closing braces
+            if (currentChar !== matches[stack.pop()]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0; // any unclosed braces left?
+}
