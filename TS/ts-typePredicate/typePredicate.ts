03fcc050b080f3f1d52e3e389f9f61811bd91867
Mark Rasavong <rasavong.mark@gmail.com>
2022-04-25 18:16:06 +0200

    organize typescript lesson demos in seprate folder

--- TS/ts-typePredicate/typePredicate.ts
+++ TS/ts-typePredicate/typePredicate.ts
@@ -1,3 +1,7 @@
+//unknown types allows us to reduce our use of any and create more strongly typed code.
+//it requires more code but it adds extra safe guarding
+
+//returns a promise type of unknown
 async function getData(path: string): Promise<unknown> {
   const response = await fetch(path);
   return await response.json();
@@ -12,10 +16,15 @@ function isPerson(person: any): person is Person {
   return "id" in person && "name" in person;
 }
 
+//returns a promise of type Person or null
 async function getPerson(id: string): Promise<Person | null> {
+  // fetch object from api
   const person = await getData("/people/1");
+  //if just person we have to make sure that  the object contains an id and name in person
   if (person && isPerson(person)) {
     return person;
   }
+
+  //else  return nothing
   return null;
 }
