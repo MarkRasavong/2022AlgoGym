6a77563a16c256959092889c3836789c932d9aa9
Mark Rasavong <rasavong.mark@gmail.com>
2022-04-26 17:48:23 +0200

    TS && AlgoGym apr-26

---
+++ april/apr-26/index.js
@@ -0,0 +1,9 @@
+https://www.codewars.com/kata/5208f99aee097e6552000148/train/javascript
+// complete the function
+function solution(str) {
+  return str.split(/(?=[A-Z])/g).join(' ');
+}
+
+function solution1(string) {
+  return string.replace(/([A-Z])/g, ' \$1');
+}
