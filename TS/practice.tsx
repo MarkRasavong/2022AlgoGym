64ac3171d0c28817af3b9c9510d168378b050e7b
Mark Rasavong <rasavong.mark@gmail.com>
2022-06-01 12:54:18 +0200

    algoGym june 1 disemvowel

--- TS/practice.tsx
+++ TS/practice.tsx
@@ -1,4 +1,15 @@
 const multiple = (n1: unknown, n2: unknown) => {
   if( typeof n1 === 'number' && typeof n2 === 'number') return n1 * n2;
   return
-}
+};
+
+const incrementString = (string: string): string =>{
+  return string.replace(/[a-b]/g, match => {
+    if(!match) return '1';
+
+    const parsedNum = String(parseInt(match) + 1);
+    const diff = match.length - parsedNum.length;
+
+    return diff > 0 ? '0'.repeat(diff) + parsedNum : parsedNum
+  })
+} 
---
+++ june/1-june/index.js
@@ -0,0 +1,3 @@
+// https://www.codewars.com/kata/52fba66badcd10859f00097e/javascript
+
+const disemvowel = str => str.replace(/[aeiou]/ig, '');
