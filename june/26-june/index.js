3608de7569630bd3a85fbe1bbeb5b7c9046ed1d6
Mark Rasavong <rasavong.mark@gmail.com>
2022-06-26 12:05:04 +0200

    leetcode june 26

---
+++ june/26-june/index.js
@@ -0,0 +1,13 @@
+//https://leetcode.com/study-plan/data-structure/?progress=souuje2
+
+var maxSubArray = function (nums) {
+  for (let i = 1; i < nums.length; i++) {
+    nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
+  }
+  return Math.max(...nums);
+};
+
+/* 
+Runtime: 187 ms, faster than 5.11% of JavaScript online submissions for Maximum Subarray.
+Memory Usage: 50.3 MB, less than 54.64% of JavaScript online submissions for Maximum Subarray.
+*/
