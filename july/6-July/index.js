28bb681beac859140f719704103a8f07acba01e4
Mark Rasavong <rasavong.mark@gmail.com>
2022-07-11 09:18:00 +0200

    algoGym july 11

--- july/6-July/index.js
+++ july/6-July/index.js
@@ -1,7 +1,3 @@
 //https://www.codewars.com/kata/57f781872e3d8ca2a000007e/
 
 const maps = x => x.map(n => n * 2);
-
-string.Prototype.toJadenCase = function () {
-  return this.split(' ').map(wrd => wrd[0].toUpperCase() + wrd.slice(1))
-}
