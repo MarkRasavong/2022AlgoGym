be70b4a2f6084c45d4f528c4e563ed5cdddfc898
Mark Rasavong <rasavong.mark@gmail.com>
2022-07-13 07:58:15 +0200

    algoGym 13 july

---
+++ july/13-july/index.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/5601409514fc93442500010b/train/javascript
+
+const betterThanAverage = (classPoints, yourPoints) => {
+  classPoints = classPoints.reduce((a, c) => a + c) / classPoints.length
+
+  return yourPoints < classPoints ? false : true
+}
