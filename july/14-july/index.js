7919ab13c199a5cc0dcd13a762e48d3032bacf87
Mark Rasavong <rasavong.mark@gmail.com>
2022-07-14 10:00:39 +0200

    algoGym july 14 v2

--- july/14-july/index.js
+++ july/14-july/index.js
@@ -1,1 +1,6 @@
+//https://www.codewars.com/kata/5648b12ce68d9daa6b000099/train/javascript
+
 const number = (busStops) => busStops.map(([entr, out]) => entr - out).reduce((acc, curr) => acc + curr)
+
+
+const numberClever = busStops => busStops.reduce((n, [on, off]) => n + on - off, 0);
